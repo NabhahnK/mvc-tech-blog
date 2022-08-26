2c18c4ac665c1b6d14c1a778768b9b711a9f39ca
Nabhahn Kamaal <NabhahnK@gmail.com>
2022-08-26 15:38:06 -0700

    got rid of extra html in main.handlebars

--- controllers/index.js
+++ controllers/index.js
@@ -2,11 +2,9 @@ const router = require('express').Router();
 const apiRoutes = require('./api');
 const homeRoutes = require('./homeRoutes');
 const profileRoutes = require('./profileRoutes');
-const commentRoutes = require('./commentRoutes');
 
 router.use('/api', apiRoutes);
 router.use('/', homeRoutes);
 router.use('/profile', profileRoutes);
-router.use('/comment', commentRoutes);
 
 module.exports = router;
